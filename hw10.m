0f4841c75875dd7eadd0673dec554360cae92dda
iburistu <zachary.linkletter@maine.edu>
2018-09-19 00:37:52 -0400

    Updating to most recent homework 10

---
+++ hw10.m
@@ -0,0 +1,136 @@
+% Zachary Linkletter
+% ECE 498 HW 10
+% 4/30/18
+
+function varargout = hw10(varargin)
+% HW10 MATLAB code for hw10.fig
+%      HW10, by itself, creates a new HW10 or raises the existing
+%      singleton*.
+%
+%      H = HW10 returns the handle to a new HW10 or the handle to
+%      the existing singleton*.
+%
+%      HW10('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in HW10.M with the given input arguments.
+%
+%      HW10('Property','Value',...) creates a new HW10 or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before hw10_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to hw10_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help hw10
+
+% Last Modified by GUIDE v2.5 23-Apr-2018 13:26:01
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @hw10_OpeningFcn, ...
+                   'gui_OutputFcn',  @hw10_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before hw10 is made visible.
+function hw10_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to hw10 (see VARARGIN)
+
+% Choose default command line output for hw10
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes hw10 wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = hw10_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+edit1string = get(handles.edit1, 'string');
+edit2string = get(handles.edit2, 'string');
+added = str2num(edit1string) + str2num(edit2string);
+
+displaystring = num2str(added);
+set(handles.text3, 'String', displaystring);
+
+
+function edit1_Callback(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit1 as text
+%        str2double(get(hObject,'String')) returns contents of edit1 as a double
+
+
+
+% --- Executes during object creation, after setting all properties.
+function edit1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function edit2_Callback(hObject, eventdata, handles)
+% hObject    handle to edit2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit2 as text
+%        str2double(get(hObject,'String')) returns contents of edit2 as a double
+
+% --- Executes during object creation, after setting all properties.
+function edit2_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
--- hw9.m
+++ hw9.m
@@ -1,31 +1,39 @@
 % Zachary Linkletter
 % ECE 498 HW 9
-% 4/16/18
+% 4/23/18
 
 clear
 clc
 
 % Question 1
+f = [143; 69];
+A = [110 30
+     120 210
+     1 1];
+b = [4000; 15000; 75];
+lb = zeros(2,1);
+[Q1,fval,exitflag,output,lambda] = linprog(-f,A,b,[],[],lb);
+Q1
 
-profit = @(x) 143*x(1) + 69*x(2);
-
-
-
+% Question 2
+f = [4; 5];
+A = [1 1
+     1.25 .75];
+b = [200; 200];
+ub = [inf; 150];
+[Q2,fval,exitflag,output,lambda] = linprog(-f,A,b,[],[],[],ub);
+Q2
 
 % Question 3
 x0 = [0,0,0];
 fun = @(x) (x(1)^2 + x(2)^2)^2 - x(1)^2 - x(2) + x(3)^2;
 [Q3,y] = fminunc(fun, x0);
-
 Q3
 
 % Question 4
 A = [1, -1];
 B = [2];
 x0 = [0,0];
-
 fun2 = @(x) 2 * x(1)^2 + 20 * x(2)^2 + 6 * x(1) * x(2) + 5 * x(1);
-
 [Q4, y] = fmincon(fun2, x0, A, B);
-
 Q4
